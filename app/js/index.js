a251489ed443ee78b76ee86535d15c377beb1b38
Zimzik <mrzimko@gmail.com>
2017-05-19 15:41:35 +0300

    Add responsive main menu

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,7 @@
-console.log("Hi!");
+let hamburger = document.querySelector('.hamburger'),
+    navMenu = document.querySelector('.main-navigation ul');
+
+hamburger.addEventListener("click", function() {
+  this.classList.toggle("is-active");
+  navMenu.classList.toggle("display-menu");
+});
